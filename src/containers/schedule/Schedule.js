4ba504b449071d22834cb5b1f8d1ad7a356a3d3a
Gerald Heng <geralddevelop@gmail.com>
2022-07-03 21:00:17 +0800

    feat: :bug:

--- src/containers/schedule/Schedule.js
+++ src/containers/schedule/Schedule.js
@@ -3,7 +3,7 @@ import Content from "../commons/Content";
 export default function Schedule() {
   return (
     <div id="schedule">
-      <Content title="Schedule">THis is some schedule</Content>
+      <Content title="Schedule">THis is some schedule</Content> 
     </div>
   );
 }
